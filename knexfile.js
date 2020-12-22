d566dc657c0670256677ee377c11b9c40f2e9e5a
Joe-Thompson <bmo3696@gmail.com>
2020-12-22 09:31:37 -0500

    ssl setup

--- knexfile.js
+++ knexfile.js
@@ -35,16 +35,11 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.HOST,
-      user: process.env.USER,
-      port: process.env.PORT,
-      database: process.env.DATABASE,
-      password: process.env.PASSWORD
-    },
+    connection: "postgres://bwlvlrdsgrvzai:caae7f88489b3725cacdc3c2faa5fa541234f90476b1232eaf859f01d2504a66@ec2-3-211-167-220.compute-1.amazonaws.com:5432/du47n12bsumd5",
     useNullAsDefault: true,
     ssl: {
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
+      disable: true
     },
     migrations: {
       directory: './database/migrations'
