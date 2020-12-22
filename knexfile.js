b414067c071efe29fa927c358b42db51bb5ec4d2
Joe-Thompson <bmo3696@gmail.com>
2020-12-22 17:37:23 -0500

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -35,14 +35,10 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: "postgres://ajxfomojdzickj:b3a7caf4f3d5584ad3d40a9de9e8315e7e88723541a56f12917859266600d4a3@ec2-3-229-51-131.compute-1.amazonaws.com:5432/d5gddvr25lbjqu",
+    connection: process.env["DATABASE_URL "],
     useNullAsDefault: true,
-    ssl: {
-      rejectUnauthorized: false,
-      disable: true
-    },
+  },
     migrations: {
       directory: './database/migrations'
     }
-  }
 };
